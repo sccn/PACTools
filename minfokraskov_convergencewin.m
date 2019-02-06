edd91bc1d07e9d27f61aadfe25871ea48a83ede9
ramonmc <ram033@eng.ucsd.edu>
2019-02-05 22:51:44 -0800

    Help message added. Normalization of data was implemented.

--- minfokraskov_convergencewin.m
+++ minfokraskov_convergencewin.m
@@ -1,40 +1,121 @@
-function  [I,Ilocal,kconv,difvarvect, totalIlocal] = minfokraskov_convergencewin(Xorig,Yorig,varargin)
+% minfokraskov_convergencewin() - Compute pairwise local mutual information using Kraskov method
+%                                 with modifications to achieve convergence-like while
+%                                 iterating through values of 'k'. The function also allow 
+%                                 the inputt of extended data inorder to boost the neighbors count 
+% Usage:
+%   >>  pac = minfokraskov_convergencewin(X,Y);
+%
+% Inputs:
+%  Xorig        - Vector of signal X
+%  Yorig        - Vector of signal Y
+%
+% Optional inputs:
+%  'k0'            -  [integer] Number of neighbors for in the Kraskov algotithm.
+%                     If iterative method to compute local MI local is used the first 
+%                     iteration for all values of 'k' will start in 'k0'. 
+%  'k'             -  [integer] Number of neighbors for in the Kraskov algotithm.
+%                     If no iterative method to compute local MI is used the magnitude 
+%                     will be computed using 'k'number of neighbors
+%  'kraskovmethod' -  [1,2] Kraskov method to use in the computtaion on local MI. 
+%                     Default: [1]           
+%  'xdistmethod'   -  {'circ', 'myeucl','seuclid'} Method to compute distances among samples X.
+%                     Default: 'seuclid' (standardized euclidean)
+%  'ydistmethod'   -  ('circ', 'myeucl','seuclid') Method to compute distances among samples Y. 
+%                     Default: 'seuclid' (standardized euclidean)
+%  'xvarnorm_circ' -  [0,1] Flag to activate circular normalization of the
+%                     norm for X. May be nececary if circular magnitude like
+%                     phase is used
+%  'yvarnorm_circ' -  [0,1] Flag to activate circular normalization of the
+%                     norm for Y. May be nececary if circular magnitude like
+%                     phase is used
+%  'varthresh'     -  [0.01:0.5] Threshold of the percent decrease of variance if
+%                     iterative method to compute MI local is used. Default: 0.5               
+%  'kstep'         -  [integer] Step to increase the number of neighbors 'k' in case 
+%                     iterative method to compute MI local is used. Default: 1                  
+%  'maxk'          -  Maximun value of 'k' in case iterative method to compute 
+%                     MI local is used. Default 100.
+%  'saveallmi'     - [0,1] Flag to output(1) or not(0) all the vectors of
+%                    local MI correspoding to each value of 'k'. Default: [0]
+%  'normmethod'    -  ('norm' 'zscore', 'none ') Normalization method to
+%                     use for X and Y. Default: 'norm'
+%  'scaledistmat'  - [0,1] Flag to perform (1) or not (0) scaling of
+%                    distance matrix. Default: 0
+% Outputs:
+%   I              - Mutual Information
+%   Ilocal         - Local Mutual Information
+%   kconv          - k value for convergence(iterative case only)
+%   difvarvect     - Decrease of variance for each value of k used in the
+%                    iterative proccess.
+%   AllILocal      - All vectors of Local MI for each 'k' value used in
+%                    the interations. Empty if 'saveallmi'  is 0
+% See also:
+%
+% Author: Ramon Martinez-Cancino, SCCN, 2019
+%
+% Copyright (C) 2019  Ramon Martinez-Cancino,INC, SCCN
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+function  [I,Ilocal,kconv,difvarvect, AllILocal] = minfokraskov_convergencewin(Xorig,Yorig,varargin)
 % ADD HELP here
 % Xorig,Yorig colums vectors for single trials
 
 if nargin < 3
-    help minfokraskov;
+    help minfokraskov_convergencewin;
     return;
 end
 
 try
     options = varargin;
-    if ~isempty( varargin ),
+    if ~isempty( varargin )
         for i = 1:2:numel(options)
             g.(options{i}) = options{i+1};
         end
-    else g= []; end;
+    else, g= []; end
 catch
-    disp('minfokraskov() error: calling convention {''key'', value, ... } error'); return;
-end;
-try g.k0;                catch, g.k0  = 1;                        end;
-try g.k;                 catch, g.k   = [];                       end;
-try g.kraskovmethod;     catch, g.kraskovmethod   = 1;            end;
-try g.xdistmethod;       catch, g.xdistmethod     = 'seuclidean'; end;
-try g.ydistmethod;       catch, g.ydistmethod     = 'seuclidean'; end;
-try g.jointdistmethod;   catch, g.jointdistmethod = 'seuclidean'; end;
-try g.yvarnorm_circ;     catch, g.yvarnorm_circ   = 0;            end;
-try g.xvarnorm_circ;     catch, g.xvarnorm_circ   = 0;            end;
-try g.varthresh;         catch, g.varthresh       = 0.05;         end;
-try g.kstep;             catch, g.kstep           = 1;            end;
-try g.saveItot;          catch, g.saveItot        = 0;            end;
-try g.maxk;              catch, g.maxk            = 40;           end;
+    disp('minfokraskov_convergencewin() error: calling convention {''key'', value, ... } error'); return;
+end
+try g.k0;                catch, g.k0              = 1;            end
+try g.k;                 catch, g.k               = [];           end
+try g.kraskovmethod;     catch, g.kraskovmethod   = 1;            end
+try g.xdistmethod;       catch, g.xdistmethod     = 'seuclidean'; end
+try g.ydistmethod;       catch, g.ydistmethod     = 'seuclidean'; end
+try g.yvarnorm_circ;     catch, g.yvarnorm_circ   = 0;            end
+try g.xvarnorm_circ;     catch, g.xvarnorm_circ   = 0;            end
+try g.varthresh;         catch, g.varthresh       = 0.05;         end
+try g.kstep;             catch, g.kstep           = 1;            end
+try g.saveallmi;         catch, g.saveallmi       = 0;            end
+try g.maxk;              catch, g.maxk            = 40;           end
+try g.normmethod;        catch, g.normmethod      = 'norm';       end 
+try g.scaledistmat;      catch, g.scaledistmat    = 0;            end
 
 % Check dimension of signals
 X         = Xorig(:);
 Y         = Yorig(:);
 nlat      = size(X,1);
 nlatsorig = size(Xorig,1);
+
+% Normalizing data
+switch g.normmethod
+    case  'norm'
+        X = X./norm(X);
+        Y = Y./norm(Y); 
+    case 'zscore'
+        X = zscore(X);
+        Y = zscore(Y);
+end
 
 % Compute distances
 if strncmpi(g.xdistmethod,'circ',4)
@@ -68,9 +149,10 @@ else
     tmparraydist = pdist(Y,g.ydistmethod);
     dynorm = squareform(tmparraydist);
 end
-
-dxnorm = dxnorm/max(max(dxnorm));
-dynorm = dynorm/max(max(dynorm));
+if g.scaledistmat
+    dxnorm = dxnorm/max(max(dxnorm));
+    dynorm = dynorm/max(max(dynorm));
+end
 dz = max(dxnorm,dynorm);
 
 dxi = dxnorm;
@@ -94,7 +176,7 @@ end
 counter     = 1;
 difvar      = Inf;
 difvarvect  = [];
-totalIlocal = [];
+AllILocal = [];
 
 % Reducing the dim to the one in the first trial
 knntmp = knntmp(1:nlatsorig,:);
@@ -127,14 +209,12 @@ while difvar >= g.varthresh && g.k < g.maxk
     difvarvect(counter) = difvar;
     Ilocalvark          = var(Ilocal);
     g.k                 = g.k + g.kstep;
-    if g.saveItot
+    if g.saveallmi
          if g.k ==1
-             totalIlocal = Ilocal;
+             AllILocal = Ilocal;
          else
-             totalIlocal(counter,:)= Ilocal;
+             AllILocal(counter,:)= Ilocal;
          end
-             
-        %totalIlocal{counter}= Ilocal;
     end
     counter = counter + 1;
     
