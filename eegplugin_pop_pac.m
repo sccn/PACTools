1b94d8720e93a374dec2d7167e23a608c779bd69
ramonmc <ram033@eng.ucsd.edu>
2019-07-02 13:25:56 +0200

    log scale added to gui. GUI modified to accomodate new items

--- eegplugin_pop_pac.m
+++ eegplugin_pop_pac.m
@@ -1,7 +1,7 @@
 function vers = eegplugin_pop_pac(fig,  try_strings, catch_strings);
 vers = 'beta03';
 plotmenu = findobj(fig, 'tag', 'tools');
-submenu = uimenu( plotmenu, 'Label', 'ERPAC Tool', 'separator', 'on');
+submenu = uimenu( plotmenu, 'Label', 'PAC Tools', 'separator', 'on');
 uimenu( submenu, 'label', 'Estimate PAC','callback',...
     [try_strings.no_check '[EEG LASTCOM]=pop_pac(EEG);' catch_strings.add_to_hist]);
 uimenu( submenu, 'label', 'Visualize PAC','callback',...
