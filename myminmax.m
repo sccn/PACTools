ff578fb16f69304794a0c332cad95b8cb1f45f04
Ramon Martinez-Cancino <5943254+nucleuscub@users.noreply.github.com>
2021-06-14 13:29:51 +0200

    New funtion added as an alternative to native minmax

---
+++ myminmax.m
@@ -0,0 +1,29 @@
+% myminmax() - Return minimun and maximun value. If P is a bidimensional
+%              matrix with dimension P by Q, myminmax()retuns a P x 2 
+%              array of minumun
+% and maximun values for each row of P.
+%
+% Author: Ramon Martinez-Cancino
+%
+% Copyright (C) 2021  Ramon Martinez-Cancino
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+function pr = myminmax(P)
+
+minval = min(P,[], 2);
+maxval = max(P, [], 2);
+pr = [minval, maxval];
+
